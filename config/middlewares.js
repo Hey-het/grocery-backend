0daa390a96a8af8a048c40697433df9a6dfcf157
Hey-het <hetalbpatelsw@gmail.com>
2025-04-12 17:32:26 +0100

    latest updatecart code

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,13 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'https://grocery-store-g8pk-git-main-hey-hets-projects.vercel.app/'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
